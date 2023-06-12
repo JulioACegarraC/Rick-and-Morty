be0ef06f9d3bb90081424006067c285b13c5e618
JulioACegarraC <juliocegarra19@gmail.com>
2023-06-12 06:32:46 -0500

    Commit AsyncAwait

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,17 +1,16 @@
 const axios = require('axios');
 const URL = "https://rickandmortyapi.com/api/character/";
 
-function getCharById (req,res) {
-    const { id } = req.params;
-    axios.get(URL+id)
-        .then( response => {
+async function getCharById (req,res) {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(URL+id);
         const { name, status,gender,species,image,origin,location } = response.data;
-        const character = {id,name,status,gender,species,image,origin,location}
-            if(!character.name) return res.status(404).send(`Not Found`);
-            return res.status(200).json(character);       
-        })
-        .catch( error => {
-            return res.status(500).send(`${error.message}`);
-        }); 
+        const character = {id,name,status,gender,species,image,origin,location};
+        if(!character.name) return res.status(404).send(`Not Found`);
+        return res.status(200).json(character);
+    } catch (error) {
+        return res.status(500).send(error.message);   
+    }
 }
 module.exports = getCharById;
--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -1,12 +1,10 @@
 let myFavorites = [];
 function postFav (req,res) {
-    console.log(myFavorites);
     myFavorites.push(req.body)
     return res.status(200).json(myFavorites);
 }
 function deleteFav (req,res) {
     const { id } = req.params;
-    console.log(id)
     let filtered = myFavorites.filter(element => element.id !== id);
     myFavorites = filtered;
     return res.json(myFavorites);
